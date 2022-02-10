a2606bd696be67246933deb4f89be905f7facca9
Harpreet Singh <harpreet.singh17@dcmail.ca>
2022-02-10 06:39:26 -0500

    changes in footer

--- themes/dahj/footer.php
+++ themes/dahj/footer.php
@@ -15,7 +15,7 @@
 		<div class="site-info">
 				<?php
 				/* translators: 1: Copyright, 2: Theme name. */
-				printf( esc_html__( 'Copyright %1$s by %2$s.', 'dahj' ), '2022', '<a href="'.home_url.'." >Dahj Style</a>' );
+				printf( esc_html__( 'Copyright %1$s by %2$s.', 'dahj' ), '2022', '<a href="'.home_url.'" >Dahj Style</a>' );
 				?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
