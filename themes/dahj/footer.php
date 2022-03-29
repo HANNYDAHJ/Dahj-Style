c6cdbdd1f71a3f2a3d8b0b756144bba00078b8a9
Harpreet Singh <harpreet.singh17@dcmail.ca>
2022-03-29 01:03:22 -0400

    added wp-query as achieve

--- themes/dahj/footer.php
+++ themes/dahj/footer.php
@@ -12,6 +12,42 @@
 ?>
 
 	<footer id="colophon" class="site-footer">
+
+	<?php
+$recipe_args = array(
+	'post_type'              => array('dahj_recipe'), // use any for any kind of post type, custom post type slug for custom post type
+    'post_status'            => 'publish', // Also support: pending, draft, auto-draft, future, private, inherit, trash, any
+    'posts_per_page'         => 3, // use -1 for all post
+    'orderby'                => 'rand',
+);
+$recipe_query = new WP_Query($recipe_args);
+
+if ($recipe_query->have_posts()){
+
+	?>
+	<div class="grid-container">
+		<div class="grid-x grid-margin-x grid-margin-y">
+		<?php
+	while ( $recipe_query->have_posts()){
+		$recipe_query->the_post();
+		?>
+		<div class="cell small-12 medium-4">
+		<?php
+		the_post_thumbnail();
+		the_title( '<h3>','</h3>');
+		the_excerpt(  );
+		the_permalink( );
+		?>
+		</div>
+		<?php
+	}
+	wp_reset_postdata();
+	?>
+		</div>
+	</div>
+
+	<?php } ?>
+	
 		<div class="site-info">
 				<?php
 				/* translators: 1: Copyright, 2: Theme name. */
