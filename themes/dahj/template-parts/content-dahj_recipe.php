d25153cb73eb9168fb6d55aa8adfcf0066c2a058
Harpreet Singh <harpreet.singh17@dcmail.ca>
2022-03-29 15:29:49 -0400

    fixed code for content_recipe

--- themes/dahj/template-parts/content-dahj_recipe.php
+++ themes/dahj/template-parts/content-dahj_recipe.php
@@ -10,8 +10,8 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php
+    <header class="entry-header">
+        <?php
 		if (!is_singular ('product')){
 		if (is_singular() ) :
 			the_title( '<h1 class="entry-title">', '</h1>' );
@@ -21,19 +21,20 @@
 	}
 		if ( 'post' === get_post_type() ) :
 			?>
-			<div class="entry-meta">
-				<?php
+        <div class="entry-meta">
+            <?php
 				dahj_posted_on();
 				dahj_posted_by();
 				?>
-			</div><!-- .entry-meta -->
-		<?php endif; ?>
-	</header><!-- .entry-header -->
+        </div><!-- .entry-meta -->
+        <?php endif; ?>
+    </header><!-- .entry-header -->
 
-	<?php dahj_post_thumbnail(); ?>
 
-	<div class="entry-content">
-		<?php
+    <?php dahj_post_thumbnail(); ?>
+
+    <div class="entry-content">
+        <?php
 		the_content(
 			sprintf(
 				wp_kses(
@@ -56,9 +57,9 @@
 			)
 		);
 		?>
-	</div><!-- .entry-content -->
+    </div><!-- .entry-content -->
 
-	<footer class="entry-footer">
-		<?php dahj_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
+    <footer class="entry-footer">
+        <?php dahj_entry_footer(); ?>
+    </footer><!-- .entry-footer -->
 </article><!-- #post-<?php the_ID(); ?> -->
